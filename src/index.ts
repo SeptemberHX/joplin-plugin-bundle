795a0693d1f4df601535818015bbba72bff7da66
SeptemberHX <september_hx@outlook.com>
2022-09-03 20:59:41 +0800

    continue development

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ joplin.plugins.register({
 			outlinePlugin,
 			todolistPlugin,
 			dailyNotePlugin,
-			// writingMarkerPlugin,
+			writingMarkerPlugin,
 			historyPlugin,
 			// noteLinkPlugin,
 			// aggregateSearchPlugin
