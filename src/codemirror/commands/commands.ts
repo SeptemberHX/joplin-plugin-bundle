d8b8e408ee28e49ce3b60fa25bf1fe0b28ab5efd
SeptemberHX <september_hx@outlook.com>
2022-09-04 20:21:32 +0800

    scrollToLine

--- src/codemirror/commands/commands.ts
+++ src/codemirror/commands/commands.ts
@@ -5,6 +5,10 @@ export default class CommandsBridge {
     private readonly doc = this.editor.getDoc();
 
     scrollToLine(line: number): void {
-        this.editor.scrollIntoView({line :line, ch: 0}, 500);
+        // temporary fix: sometimes the first coordinate is incorrect,
+        // resulting in a difference about +- 10 px,
+        // call the scroll function twice fixes the problem.
+        this.editor.scrollTo(null, this.editor.charCoords({ line: line, ch: 0 }, 'local').top);
+        this.editor.scrollTo(null, this.editor.charCoords({ line: line, ch: 0 }, 'local').top);
     }
 }
